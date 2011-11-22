381e17c4221ef68cadfc31be0b452b58b36804dd
CalebKM <calebkm@Iapetus.local>
2011-11-21 20:08:09 -0800

    saving current progress

--- bookmarkers.rb
+++ bookmarkers.rb
@@ -1,5 +1,42 @@
 require 'sinatra'
+require 'yaml'
 
 get '/' do
   haml :index, :layout => :template
+end
+
+get '/delete' do
+  if params[:type]
+    case params[:type]
+    when 'widget'
+      if params[:index]
+        Database.delete_widget(params[:index])
+      end
+    when 'bookmarker'
+      if params[:w_index] and params[:b_index]
+        Database.delete_bookmarker(params[:w_index], params[:b_index])
+      end
+    end
+  end
+  
+  redirect '/'
+end
+
+get '/add' do
+  if params[:type]
+    case params[:type]
+    when 'widget'
+      if params[:title] and params[:title] != ""
+        widget = Widget.new(params[:title])
+        Database.add_widget(widget)
+      end
+    when 'bookmarker'
+      if params[:index] and params[:url] and params[:url] != ""
+        bookmarker = Bookmarker.new(params[:url], params[:title], params[:note])
+        Database.add_bookmarker(params[:index], bookmarker)
+      end
+    end
+  end
+  
+  redirect '/'
 end
