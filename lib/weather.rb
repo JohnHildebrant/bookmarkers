f15eaaff898811cd58cc1e020112c70568d224fc
CalebKM <calebkm@Iapetus.local>
2011-11-21 22:54:35 -0800

    saving current progress

--- lib/weather.rb
+++ lib/weather.rb
@@ -10,22 +10,34 @@ class Weather
   def forecast
     if connected? 
       forecast = @barometer.measure.forecast
-      forecast.each { |day| day.icon = get_weather_icon(day.icon) }
+      forecast.each { |day| day.icon = get_weather_icon(:small, day.icon) }
       forecast
     else
       "Can't connect to service"
     end
   end
   
-  def get_weather_icon(string)
-    url = "<img src="
+  def now
+    now = @barometer.measure.current
+    now.icon = get_weather_icon(:big, today.icon)
+    now
+  end
+  
+  def today
+    @barometer.measure.today
+  end
+  
+  def get_weather_icon(size, string)
+    url = "<img src='/images/"+size.to_s+"/"
     case string
       when 'rain'
-        url << "'rain_icon' alt='rainy' />"
+        url << "rainy.png' alt='rainy' />"
       when 'partlycloudy'
         url << "'partlycloudy_icon' alt='partly cloudy' />"
       when 'mostlycloudy'
         url << "'mostlycloyd_icon' alt='mostly cloudy' />"
+      when 'cloudy'
+        url << "cloudy.png' alt='cloudy' />"
       when 'chancesnow'
         url << "'chancesnow_icon' alt='chance of snow' />"
       else
